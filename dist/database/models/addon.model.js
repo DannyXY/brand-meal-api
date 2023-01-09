66cef754bfa24e7f020061aa0052251ac29e7707
Adewambe Daniel <danoscolobo@gmail.com>
2023-01-09 01:14:20 +0100

    vercel-settings

--- dist/database/models/addon.model.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const base_model_1 = require("./base.model");
-class AddonModel extends base_model_1.BaseModel {
-}
-AddonModel.tableName = "addons";
-exports.AddonModel = AddonModel;
-//# sourceMappingURL=addon.model.js.map
